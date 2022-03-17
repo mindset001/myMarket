3647bc18325ef585c70e2286c8aef797b5c6ffab
Shittu-Moshood <shittumoshoodademola@gmail.com>
2022-03-17 13:01:47 +0100

    market

---
+++ src/data/items.js
@@ -0,0 +1,39 @@
+const items = [
+    {
+        id:1,
+        name: "Comfy Slippers",
+        price: 12.00,
+        category: 'Footwear',
+        description: "lorem ipsum dolo sit amet",
+    },
+    {
+        id:2,
+        name: "Sneakers",
+        price: 12.00,
+        category: 'Footwear',
+        description: "lorem ipsum dolo sit amet",
+    },
+    {
+        id:3,
+        name: "Sanders",
+        price: 12.00,
+        category: 'Footwear',
+        description: "lorem ipsum dolo sit amet",
+    },
+    {
+        id:4,
+        name: "Comfy Slippers",
+        price: 12.00,
+        category: 'Footwear',
+        description: "lorem ipsum dolo sit amet",
+    },
+    {
+        id:5,
+        name: "Comfy Slippers",
+        price: 12.00,
+        category: 'Footwear',
+        description: "lorem ipsum dolo sit amet",
+    },
+]
+
+export default items
